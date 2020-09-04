d58b3c5d342f08c2ad718927b98b7bb3ebd2b095
gavole <bethwelifedha@gmail.com>
2020-09-04 17:55:17 +0300

    updated files

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
+def resume():
+    return render_template ("resume.html")
+
+
+@app.route("/index")
 def hello():
     return render_template ("index.html")
 
@@ -9,9 +15,6 @@ def hello():
 def about():
     return render_template ("about.html")
 
-@app.route("/resume")
-def resume():
-    return render_template ("resume.html")
 
 @app.route("/projects")
 def projects():
